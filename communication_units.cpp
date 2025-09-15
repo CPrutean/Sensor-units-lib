35a0a5aea05e4695ed342adbe705b1bbd266deed
Calin Prutean <Calin.prutean@gmail.com>
2025-09-14 22:09:48 -0700

    Fixed error with const variables

--- communication_units.cpp
+++ communication_units.cpp
@@ -6,7 +6,7 @@ char piBufferSeper[] {'\n', '\0'};
 
 
 
-void stageForReturn(const char* str) {
+void stageForReturn(char* str) {
     strncat(str, piBufferSeper, sizeof(str)-strlen(str));
     Serial.print(str);
 }
